3e6ee7a1d2d4999288cfabb1155fd94737772eba
sam217pa <samuel.barreto8@gmail.com>
2016-04-29 18:13:13 +0200

    fixed color and add annotations

--- R/plot_align.R
+++ R/plot_align.R
@@ -9,26 +9,28 @@
 #' @author Samuel Barreto
 #' @import dplyr
 #' @import ggplot2
+#' @import cowplot
 #' @importFrom RColorBrewer brewer.pal
 #' @importFrom ggthemes extended_range_breaks
-#' @importFrom assertthat assert_that is.string
+#' @importFrom assertthat assert_that is.string is.flag
 #' @export
-plot_align <- function(data, mutant_, plot_title=NULL, quality = 30)
+plot_align <- function(data, mutant_, plot_title=NULL, quality = 30, color=TRUE)
 {
     assert_that(
-        is.data.frame(data),
-        is.string(mutant_),
-        is.null(plot_title) | is.string(plot_title)
+        is.data.frame(data)
+       ,is.string(mutant_)
+       ,is.null(plot_title) | is.string(plot_title)
+       ,is.flag(color)
     )
 
     if (is.null(plot_title))
         plot_title <- paste("Alignement pour la manip", toupper(mutant_))
 
     ## default color
-    red    <- brewer.pal(n = 4, "Set1")[1]
-    blue   <- brewer.pal(n = 4, "Set1")[2]
-    green  <- brewer.pal(n = 4, "Set1")[3]
-    violet <- brewer.pal(n = 4, "Set1")[4]
+    rec.col  <- "#E45052" # red paler than Set1
+    don.col  <- "#72BCE4"
+    rest.col <- brewer.pal(n = 4, "Spectral")[3]
+    ## gc.col  <- brewer.pal(n = 4, "Spectral")[4]
 
     sort_by_tract_length <- function(data, mut)
     {
@@ -51,7 +53,9 @@ plot_align <- function(data, mutant_, plot_title=NULL, quality = 30)
         filter(mutant %in% mutant_) %>%
         keep_clean_only() %>%
         sort_by_tract_length() %>%
-        filter(cons == "x" | cons == "X")
+        filter(cons == "x" | cons == "X") %>%
+        rowwise() %>%
+        mutate(expb = ifelse(expb %in% c("A", "T"), "w", "s"))
 
     first_snp <- filter(data, cons == "X") %>% summarise(min = min(refp))
     last_snp <- filter(data, cons == "x") %>% summarise(max = max(refp))
@@ -62,107 +66,69 @@ plot_align <- function(data, mutant_, plot_title=NULL, quality = 30)
 
     align_plot <- ggplot(data = data, aes(x = refp, y = name)) +
         geom_point(aes(color = inconv, size = qual, alpha=qual)) +
-        ## représente les cas complexes
-        geom_point(data = filter(data, isrestor == TRUE, qual > quality),
-                   aes(size = qual),
-                   color = "gray") +
         ## représente la séquence du donneur
-        geom_text(aes(label = snpb, x = refp, y = -7),
-                  color = "black",
-                  vjust = -2,
-                  size = 4,
-                  family = "Ubuntu Light") +
-        ## annotate("text", x = as.numeric(first_snp) - 20, y = -7, vjust = -2,
-        ##          label = "Donneur : ", color = blue, size = 4 ) +
-        ## ## représente la séquence du receveur.
-        geom_text(aes(label = refb, x = refp,
-                      y = num_of_seq + 10), color = "grey", vjust = 4, size = 4,
-                  family = "Ubuntu Light") +
+        geom_text(aes(label = snpb, x = refp, y = -7), color = don.col,
+                  vjust = -2, size = 4, family = "Ubuntu Light") +
+        geom_text(aes(label = refb, x = refp, y = num_of_seq + 10)
+                 ,color = rec.col, vjust = 4, size = 4
+                 ,family = "Ubuntu Light") +
         scale_alpha( range=c(1/5, 0.8), guide=FALSE ) +
         scale_size(range = c(0.5, 2), breaks = c(10, 50),
                    labels = c("Faible", "Forte")) +
-        scale_shape(guide = FALSE) +
         scale_x_continuous(breaks = extended_range_breaks()(data$refp)) +
-        scale_color_manual(limits = c("TRUE", "FALSE", "A", "T", "C", "G"),
-                           values = c("black", "gray", blue, blue, red, red),
-                           labels = c("Donneur", "Receveur", "A", "T", "C", "G")) +
         ## du premier au dernier snp
         coord_cartesian(xlim = c(first_snp, last_snp)) +
-        labs(x = "", y = "", size = "Qualité",
-             title = plot_title,
-             color = "Haplotype") +
-        guides(colour = guide_legend(override.aes = list(shape = 20, size = 3))) +
+        labs(x = "", y = "", size = "Qualité" ,title = plot_title
+            ,color = "Haplotype" ,shape = "Restauration\nvers…") +
+        guides(
+            ## change le symbol et la taille de la légende pour les couleurs
+            colour = guide_legend(override.aes = list(shape = 20, size = 3))
+            ## change la couleur par défault de la légende de taille
+           ,size  = guide_legend(override.aes = list(color = don.col))) +
         theme(legend.margin = unit(0,"lines"),
               panel.grid.major.y = element_line(size = 0.1, linetype = "dotted"),
               legend.position = "right",
               legend.justification = c(0, 1),
               legend.text = element_text(size = 6))
-        ## legend_position(0.8, 0.98)
 
-    if (nchar(mutant_) == 2) {
+    ## représente les cas de traces de conversion complexes
+    align_plot <- align_plot +
+        geom_point(data = filter(data, isrestor == TRUE, qual > quality)
+                  ,aes(x = refp + 12 ,shape = expb)
+                  ,size = 3) +
+        scale_shape_manual(breaks = c("w", "s")
+                          ,values = c("S", "W")
+                          ,labels = c("AT", "GC"))
+    ## }
+
+    if (color) {
         align_plot <- align_plot +
-            ## plot complex cases
+            ## représente les cas complexes
             geom_point(data = filter(data, isrestor == TRUE, qual > quality),
-                       aes(x = refp + 8, color = expb),
-                       size = 4, shape = "+") +
-            ## switch point
-            geom_point(aes(x = switchp + 8, color = switchb), alpha = 1, size = 4, shape = "-")
+                       aes(size = qual),
+                       color = rec.col) +
+            scale_color_manual(
+                limits = c("TRUE", "FALSE")
+               ,values = c(don.col, rec.col)
+               ,labels = c("Donneur", "Receveur"))
+
     } else {
         align_plot <- align_plot +
-            geom_point(data = filter(data, isrestor == TRUE, qual > quality),
-                       aes(x = refp + 8, color = expb),
-                       size = 4, shape = "+")
+            ## représente les cas complexes
+            geom_point(data = filter(data, isrestor == TRUE, qual > quality)
+                      ,aes(size = qual)
+                      ,color = rec.col) +
+            scale_color_manual(
+                limits = c("TRUE", "FALSE")
+               ,values = c("black", "gray")
+               ,labels = c("Donneur", "Receveur"))
+
     }
 
-    align_plot
-}
-
-#' Plot the quality of the sequence
-#'
-#' @title plot_qual
-#' @param data the output of \code{run_phruscle}.
-#' @param mutant_ the mutant to filter by.
-#' @return a ggplot2 plot.
-#' @author Samuel Barreto
-#' @importFrom RColorBrewer brewer.pal
-#' @import dplyr
-#' @import ggplot2
-#' @export
-
-plot_qual <- function(data, mutant_) {
-    red    <- brewer.pal(n = 4, "Set1")[1]
-    blue   <- brewer.pal(n = 4, "Set1")[2]
-    grey   <- brewer.pal(n = 9, "Greys")[4]
-    black  <- brewer.pal(n = 9, "Greys")[9]
-
-    data %>%
-        filter(mutant == mutant_) %>%
-        ggplot(aes(x = refp, y = qual)) +
-        geom_point(aes(color = qual), size = 1, alpha = 1/20) +
-        geom_smooth(se = FALSE, color = red, linetype = "dotted") +
-        ## scale_color_brewer(palette = "Greys") +
-        scale_color_continuous(high = grey, low = black) +
-        labs(x = "Position sur la référence", y = "")
-}
-
-
-#' Plot the alignment and the quality of the sequence juxtaposed
-#'
-#'
-#' @title plot_align_qual
-#' @param data the output of \code{run_phruscle}.
-#' @param mutant_ the mutant to filter by.
-#' @return a ggplot2 plot.
-#' @author Samuel Barreto
-#' @importFrom cowplot plot_grid
-#' @export
-
-plot_align_qual <- function(data, mutant_)
-{
-    plot_grid(
-        plot_align(data, mutant_),
-        plot_qual(data, mutant_),
-        ncol = 1, rel_heights = c(8.5, 1.5),
-        align = 'v', labels = c("  1", "  2")
-    )
+    ggdraw(align_plot) +
+        draw_label(label = "Donneur", x = 0.07, y = 0.1,
+                   fontfamily = "Ubuntu Light", size = 8, colour = don.col) +
+        draw_label(label = "Receveur", x = 0.07, y = 0.89, colour = rec.col,
+                   fontfamily = "Ubuntu Light", size = 8)
+    ## align_plot
 }
